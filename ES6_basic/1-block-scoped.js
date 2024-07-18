c66716384c0929050bee972726e7a75aac0f05f2
LS7710 <luissoto7710@gmail.com>
2024-07-17 21:56:24 -0400

    task 01 fix

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,13 +1,12 @@
 export default function taskBlock(trueOrFalse) {
     let task = false;
     let task2 = true;
-  
+
     if (trueOrFalse) {
         const innerTask = true;
         const innerTask2 = false;
         console.log(innerTask, innerTask2);
       }
-  
+
     return [task, task2];
   }
-  
