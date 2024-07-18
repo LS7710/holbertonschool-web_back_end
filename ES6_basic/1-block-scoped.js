bfc37abbc7cbf3326ad93284d51e7e61c4dcf326
LS7710 <luissoto7710@gmail.com>
2024-07-17 23:21:11 -0400

    task 01 fix again

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-        const task = false;
-        const task2 = true;
-
+        let task = false;
+        let task2 = true;
+      
         if (trueOrFalse) {
-        const innerTask = true;
-        const innerTask2 = false;
-        console.log(innerTask, innerTask2);
+          task = true;
+          task2 = false;
         }
-
+      
         return [task, task2];
-}
+      }
+      
