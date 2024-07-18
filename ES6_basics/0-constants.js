0abaacf91d5b86011f8a0f914e3ef77ccb4786f9
LS7710 <luissoto7710@gmail.com>
2024-07-17 21:23:44 -0400

    task 0

---
+++ ES6_basics/0-constants.js
@@ -0,0 +1,19 @@
+// Modify taskFirst to use const
+export function taskFirst() {
+    const task = 'I prefer const when I can.';
+    return task;
+  }
+  
+  // Keep getLast unchanged
+  export function getLast() {
+    return ' is okay';
+  }
+  
+  // Modify taskNext to use let
+  export function taskNext() {
+    let combination = 'But sometimes let';
+    combination += getLast();
+  
+    return combination;
+  }
+  
