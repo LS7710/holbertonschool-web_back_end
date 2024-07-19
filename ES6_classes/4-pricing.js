c414fb80dde44d5964a65d5234a7219ed30715a9
LS7710 <luissoto7710@gmail.com>
2024-07-19 15:13:29 -0400

    task 4 fix

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,25 +1,25 @@
 import Currency from './3-currency';
 
-class Pricing {
+export default class Pricing {
   constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
+    this._amount = Pricing._validateAmount(amount);
+    this._currency = Pricing._validateCurrency(currency);
   }
 
   get amount() {
     return this._amount;
   }
 
-  set amount(value) {
-    this._amount = value;
+  set amount(newAmount) {
+    this._amount = Pricing._validateAmount(newAmount);
   }
 
   get currency() {
     return this._currency;
   }
 
-  set currency(value) {
-    this._currency = value;
+  set currency(newCurrency) {
+    this._currency = Pricing._validateCurrency(newCurrency);
   }
 
   displayFullPrice() {
@@ -27,8 +27,23 @@ class Pricing {
   }
 
   static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('Both amount and conversionRate must be numbers');
+    }
     return amount * conversionRate;
   }
-}
 
-export default Pricing;
+  static _validateAmount(amount) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    return amount;
+  }
+
+  static _validateCurrency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+    return currency;
+  }
+}
