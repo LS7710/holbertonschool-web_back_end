d0d790cf6a03696817fc2ebeb603338c93efcb87
LS7710 <luissoto7710@gmail.com>
2024-07-19 23:19:53 -0400

    task 9 lint fix

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -12,16 +12,16 @@ export class HolbertonClass {
     return this._location;
   }
 }
- 
-  export class StudentHolberton {
-    constructor(firstName, lastName, holbertonClass) {
+
+export class StudentHolberton {
+  constructor(firstName, lastName, holbertonClass) {
     if (!(holbertonClass instanceof HolbertonClass)) {
-    throw new TypeError('holbertonClass must be an instance of HolbertonClass');
+      throw new TypeError('holbertonClass must be an instance of HolbertonClass');
     }
     this._firstName = firstName;
     this._lastName = lastName;
     this._holbertonClass = holbertonClass;
-    }
+  }
 
   get fullName() {
     return `${this._firstName} ${this._lastName}`;
@@ -36,15 +36,15 @@ export class HolbertonClass {
   }
 }
 
-  const class2019 = new HolbertonClass(2019, 'San Francisco');
-  const class2020 = new HolbertonClass(2020, 'San Francisco');
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-  const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
-  const student2 = new StudentHolberton('John', 'Doe', class2020);
-  const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
-  const student4 = new StudentHolberton('Donald', 'Bush', class2019);
-  const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+const student2 = new StudentHolberton('John', 'Doe', class2020);
+const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-  const listOfStudents = [student1, student2, student3, student4, student5];
+const listOfStudents = [student1, student2, student3, student4, student5];
 
-  export default listOfStudents;
+export default listOfStudents;
