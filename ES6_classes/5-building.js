72907530653adc294668e60dba5014c239474a28
LS7710 <luissoto7710@gmail.com>
2024-07-19 22:34:33 -0400

    task 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,7 +1,7 @@
 class Building {
     constructor(sqft) {
         this._sqft = sqft;
-        if (this.constructor !== Building && !this.evacuationWarningMessage) {
+        if (this.constructor !== Building && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
             throw new Error("Class extending Building must override evacuationWarningMessage");
         }
     }
