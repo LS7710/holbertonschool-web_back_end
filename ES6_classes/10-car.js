5cf96088ef8fca562886856f3c6e5aee33dd64e6
LS7710 <luissoto7710@gmail.com>
2024-07-19 23:30:24 -0400

    task 10 lint fix

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -11,11 +11,11 @@ export default class Car {
 
   get motor() {
     return this._motor;
-}
+  }
 
   get color() {
     return this._color;
-}
+  }
 
   cloneCar() {
     const clonedCar = Object.create(Object.getPrototypeOf(this));
