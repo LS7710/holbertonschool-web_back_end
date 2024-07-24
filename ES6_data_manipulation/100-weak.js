358778860629ff51951d3fd7f29d6c88c3e48d74
LS7710 <luissoto7710@gmail.com>
2024-07-23 22:02:12 -0400

    task 11

---
+++ ES6_data_manipulation/100-weak.js
@@ -0,0 +1,18 @@
+
+export const weakMap = new WeakMap();
+
+
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  
+  let count = weakMap.get(endpoint);
+  count += 1;
+  
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+  
+  weakMap.set(endpoint, count);
+}
