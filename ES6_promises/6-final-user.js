7a90f6f8fcc934fdc73bbc321116a1cdf00a7d14
LS7710 <luissoto7710@gmail.com>
2024-07-25 22:08:46 -0400

    task 7 fix

--- ES6_promises/6-final-user.js
+++ ES6_promises/6-final-user.js
@@ -1,5 +1,12 @@
- export default function uploadPhoto(fileName) {
-  return new Promise((_, reject) => {
-    reject(new Error(`${fileName} cannot be processed`));
-  });
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) => results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
+  })));
 }
