6d75f7b1b90e3a81edd494ca4760a9e63bc43804
LS7710 <luissoto7710@gmail.com>
2024-08-15 22:25:20 -0400

    t6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""light work"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """return"""
+    return sum(mxd_lst)
