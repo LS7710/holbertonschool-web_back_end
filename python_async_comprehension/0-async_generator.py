395f4f1ba3b476a9bca38c098f8712b50143fecb
LS7710 <luissoto7710@gmail.com>
2024-08-23 07:56:24 -0400

    task 0 fix

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,12 +5,8 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """
-    Coroutine that asynchronously generates 10 random numbers.
 
-    Yields:
-        A random float between 0 and 10.
-    """
+
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
