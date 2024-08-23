2b29fde3981f3a997651c47e5d66de69f8d170f3
LS7710 <luissoto7710@gmail.com>
2024-08-23 07:49:22 -0400

    fix 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
-"""
-Task 0
-"""
+"""Task 0"""
 import asyncio
 import random
 from typing import AsyncGenerator
 
-"""
-Func 0
-"""
+
 async def async_generator() -> AsyncGenerator[float, None, None]:
     yield random.uniform(0, 10)
