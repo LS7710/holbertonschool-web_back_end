a27b9bbb2be0f29fe371f336b591827dbb1dcefe
LS7710 <luissoto7710@gmail.com>
2024-08-23 15:22:50 -0400

    pycode :(

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -9,10 +9,6 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Calculate the start and end index for the pagination parameters.
 
-    Args:
-        page (int): The page number (1-indexed).
-        page_size (int): The number of items per page.
-
     Returns:
         tuple: A tuple containing the start index and the end index.
     """
@@ -22,16 +18,14 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -44,21 +38,16 @@ class Server:
         """
         Get a page of data from the dataset.
 
-        Args:
-            page (int): The page number (1-indexed).
-            page_size (int): The number of items per page.
-
         Returns:
             List[List]: The list of rows corresponding to the requested page.
         """
         assert isinstance(page, int) and page > 0, \
-        "Page number must be a positive integer"
+            "Page number must be a positive integer"
         assert isinstance(page_size, int) and page_size > 0, \
-        "Page size must be a positive integer"
+            "Page size must be a positive integer"
 
         start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
-
 
         if start_index >= len(dataset):
             return []
