0fb5d5b056a6ea73cea9cd15cfa5afab22e4a426
LS7710 <luissoto7710@gmail.com>
2024-08-23 16:45:18 -0400

    task 12

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Task 12"""
+
+from pymongo import MongoClient
+
+
+def log_stats():
+
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    nginx_collection = db.nginx
+
+
+    total_logs = nginx_collection.count_documents({})
+    print(f"{total_logs} logs")
+
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+
+    status_checks = nginx_collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_checks} status check")
+
+if __name__ == "__main__":
+    log_stats()
