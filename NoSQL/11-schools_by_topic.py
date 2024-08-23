cee32656a1a40dcd85c05b550f4ed6945aa33370
LS7710 <luissoto7710@gmail.com>
2024-08-23 16:42:21 -0400

    task 11

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Task 11"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns the list of schools having a specific topic.
+    """
+    return list(mongo_collection.find({"topics": topic}))
