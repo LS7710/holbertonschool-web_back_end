b1deacae59b35d4ca5385c305e91f3fe5718d6c2
LS7710 <luissoto7710@gmail.com>
2024-09-19 19:35:36 -0400

    task 2 fix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
-console.log('Welcome to Holberton School, what is your name?');
+console.log('Welcome to Holberton School, what is your name?\r');
 
 process.stdin.on('data', (input) => {
   const name = input.toString().trim();
-  console.log(`Your name is: ${name}`);
+  console.log(`Your name is: ${name}\r`);
   process.exit(0);
 });
 
 process.on('exit', () => {
-  console.log('This important software is now closing');
+  console.log('This important software is now closing\r');
 });
