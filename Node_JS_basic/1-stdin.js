22dcc7a352488fe30bfac6e4937ceffb2cdb0d80
LS7710 <luissoto7710@gmail.com>
2024-09-19 22:29:00 -0400

    fix pls work

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,9 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
-  process.exit(0);
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('readable', () => {
+  const data = process.stdin.read();
+  if (data !== null) process.stdout.write(`Your name is: ${data.toString()}`);
 });
 
-process.on('exit', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
